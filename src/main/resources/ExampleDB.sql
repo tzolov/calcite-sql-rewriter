bedd719011e6ff5a9ed352d02f87b89fe1f95ea1
Christian Tzolov <christian.tzolov@gmail.com>
2017-03-08 23:53:27 +0100

    Add extra columns. Sample Insert and Update added

--- src/main/resources/ExampleDB.sql
+++ src/main/resources/ExampleDB.sql
@@ -5,6 +5,7 @@ CREATE TABLE hr.depts_journal (
   deptno SERIAL NOT NULL,
   version_number TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   subsequent_version_number TIMESTAMP WITH TIME ZONE NULL DEFAULT NULL,
+  department_name VARCHAR NOT NULL,
   PRIMARY KEY (deptno, version_number)
 );
 
@@ -13,15 +14,18 @@ CREATE TABLE hr.emps_journal (
   deptno INT NOT NULL,
   version_number TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   subsequent_version_number TIMESTAMP WITH TIME ZONE NULL DEFAULT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
   PRIMARY KEY (empid, version_number)
 );
 
+DROP VIEW IF EXISTS hr.depts;
 CREATE VIEW hr.depts AS
   WITH link_last AS (
       SELECT *, MAX(version_number) OVER (PARTITION BY deptno) AS last_version_number
       FROM hr.depts_journal
   )
-  SELECT deptno
+  SELECT deptno, department_name
   FROM link_last
   WHERE subsequent_version_number IS NULL
         AND version_number = last_version_number;
@@ -36,8 +40,27 @@ CREATE VIEW hr.emps AS
   WHERE subsequent_version_number IS NULL
         AND version_number = last_version_number;
 
-INSERT INTO hr.depts_journal (deptno) VALUES (1),(2),(3),(4);
-INSERT INTO hr.emps_journal (empid,deptno) VALUES (1,1),(2,1),(3,2),(4,2),(5,2),(6,4);
+INSERT INTO hr.depts_journal (deptno, department_name) VALUES (1, 'Dep1'),(2, 'Dep2'),(3, 'Dep3'),(4, 'Dep4');
+INSERT INTO hr.emps_journal (empid,deptno,first_name,last_name) VALUES (1,1,'Peter','Pan'),(2,1,'Ian','Bibian'),(3,2,'Victor','Strugatski'),(4,2,'Stan','Ban'),(5,2,'Dimitar','Gergov'),(6,4,'Ivan','Baraban');
 
 -- Employee 1 moves to department 2
-INSERT INTO hr.emps_journal (empid,deptno) VALUES (1,2);
+INSERT INTO hr.emps_journal (empid,deptno,first_name,last_name) VALUES (1,2,'Peter','Pan');
+
+-- INSERT
+--INSERT INTO hr.depts (deptno, department_name) VALUES(666, 'Pivotal');
+INSERT INTO hr.depts_journal (deptno, department_name) VALUES(666, 'Pivotal');
+
+-- UPDATE
+-- UPDATE hr.depts SET department_name='First' WHERE deptno = 1;
+INSERT INTO hr.depts_journal (deptno, version_number, department_name)
+  WITH link_last AS (
+    SELECT *, MAX(version_number) OVER (PARTITION BY deptno) AS last_version_number
+      FROM hr.depts_journal
+      WHERE deptno = 1
+  )
+  SELECT deptno, version_number + interval '1 hour', 'Name2'
+    FROM link_last
+    WHERE subsequent_version_number IS NULL
+        AND version_number = last_version_number;
+
+SELECT * from hr.depts;
