5da0cbcbff55445682d90d4ba262e2101b5c8aca
David Evans <devans@pivotal.io>
2017-03-09 17:08:55 +0000

    Extend integration tests to check database results, more complex query

--- src/test/java/org/apache/calcite/adapter/jdbc/JournalledSelectRuleIntegrationTest.java
+++ src/test/java/org/apache/calcite/adapter/jdbc/JournalledSelectRuleIntegrationTest.java
@@ -1,13 +1,21 @@
 package org.apache.calcite.adapter.jdbc;
 
+import com.google.common.base.Function;
 import io.pivotal.beach.calcite.programs.ForcedRulesProgram;
 import io.pivotal.beach.calcite.programs.SequenceProgram;
 import org.apache.calcite.runtime.Hook;
 import org.apache.calcite.test.CalciteAssert;
+import org.apache.calcite.tools.Program;
+import org.apache.calcite.util.Holder;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JournalledSelectRuleIntegrationTest {
+
+	private Function<Holder<Program>, Void> program = SequenceProgram.prepend(
+			new ForcedRulesProgram(new JournalledSelectRule())
+	);
+
 	@BeforeClass
 	public static void rebuildTestDatabase() throws Exception {
 		TargetDatabase.rebuild();
@@ -18,9 +26,7 @@ public class JournalledSelectRuleIntegrationTest {
 		CalciteAssert
 				.model(TargetDatabase.JOURNALLED_MODEL)
 				.query("SELECT \"empid\" FROM \"hr\".\"emps\"")
-				.withHook(Hook.PROGRAM, SequenceProgram.prepend(new ForcedRulesProgram(
-						new JournalledSelectRule()
-				)))
+				.withHook(Hook.PROGRAM, program)
 				.explainContains("PLAN=JdbcToEnumerableConverter\n" +
 						"  JdbcProject(empid=[CAST($0):INTEGER])\n" +
 						"    JdbcFilter(condition=[AND(=($1, $3), IS NULL($2))])\n" +
@@ -30,6 +36,27 @@ public class JournalledSelectRuleIntegrationTest {
 				.planHasSql("SELECT CAST(\"empid\" AS INTEGER) AS \"empid\"\n" +
 						"FROM (SELECT \"empid\", \"version_number\", \"subsequent_version_number\", MAX(\"version_number\") OVER (PARTITION BY \"empid\" ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS \"$f6\"\n" +
 						"FROM \"calcite_sql_rewriter_integration_test\".\"emps_journal\") AS \"t\"\n" +
-						"WHERE \"version_number\" = \"$f6\" AND \"subsequent_version_number\" IS NULL");
+						"WHERE \"version_number\" = \"$f6\" AND \"subsequent_version_number\" IS NULL")
+				.returns("empid=1\n" +
+						"empid=2\n" +
+						"empid=3\n" +
+						"empid=4\n" +
+						"empid=5\n" +
+						"empid=6\n");
 	}
+
+	@Test
+	public void testComplexSelectRewriting() {
+		CalciteAssert
+				.model(TargetDatabase.JOURNALLED_MODEL)
+				.query("SELECT \"d\".\"deptno\"\n" +
+						"FROM \"hr\".\"emps\" AS \"e\"\n" +
+						"JOIN \"hr\".\"depts\" AS \"d\" ON \"e\".\"deptno\" = \"d\".\"deptno\"\n" +
+						"GROUP BY \"d\".\"deptno\"\n" +
+						"HAVING COUNT(*) > 1")
+				.withHook(Hook.PROGRAM, program)
+				.runs()
+				.returns("deptno=2\n");
+	}
+
 }
