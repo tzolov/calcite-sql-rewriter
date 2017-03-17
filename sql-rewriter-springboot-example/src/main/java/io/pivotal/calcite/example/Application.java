15def858654f1480d23dc5b3d926fef77d724891
Christian Tzolov <christian.tzolov@gmail.com>
2017-03-17 09:40:10 +0100

    Inline the Calcite model configuraions with the Spring properties

--- sql-rewriter-springboot-example/src/main/java/io/pivotal/calcite/example/Application.java
+++ sql-rewriter-springboot-example/src/main/java/io/pivotal/calcite/example/Application.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
@@ -33,62 +34,83 @@ public class Application {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	@Bean
-	public CommandLineRunner demo() {
-		return (args) -> {
-			log.info("Creating tables");
+	@Override
+	public void run(String... strings) throws Exception {
+		log.info("Insert ROWs");
 
-			// INSERT
-			LongStream.range(666, 676).forEach(
-					id -> jdbcTemplate.execute("INSERT INTO hr.depts (deptno, department_name) VALUES (" + id + ", 'Dep" + id + "')")
-			);
+		LongStream.range(666, 676).forEach(
+				id -> jdbcTemplate.execute("INSERT INTO hr.depts (deptno, department_name) VALUES (" + id + ", 'Dep" + id + "')")
+		);
 
-			jdbcTemplate.query(
-					"SELECT * FROM hr.depts WHERE deptno > 668",
-					(rs, rowNum) -> new Department(rs.getLong("deptno"), rs.getString("department_name"))
-			).forEach(department -> log.info(department.toString()));
+		jdbcTemplate.query(
+				"SELECT * FROM hr.depts WHERE deptno > 668",
+				(rs, rowNum) -> new Department(rs.getLong("deptno"), rs.getString("department_name"))
+		).forEach(department -> log.info(department.toString()));
 
-			// UPDATE
-			IntStream.range(666, 676).forEach(
-					id -> jdbcTemplate.execute("UPDATE hr.depts SET department_name = 'Boza' WHERE deptno = " + id)
-			);
+		log.info("UPDATE ROWs");
+		IntStream.range(666, 676).forEach(
+				id -> jdbcTemplate.execute("UPDATE hr.depts SET department_name = 'Boza' WHERE deptno = " + id)
+		);
 
-			jdbcTemplate.query(
-					"SELECT * FROM hr.depts",
-					(rs, rowNum) -> new Department(rs.getLong("deptno"), rs.getString("department_name"))
-			).forEach(department -> log.info(department.toString()));
+		jdbcTemplate.query(
+				"SELECT * FROM hr.depts",
+				(rs, rowNum) -> new Department(rs.getLong("deptno"), rs.getString("department_name"))
+		).forEach(department -> log.info(department.toString()));
 
-			// DELETE
-			IntStream.range(666, 676).forEach(
-					id -> jdbcTemplate.execute("DELETE FROM hr.depts WHERE deptno = " + id)
-			);
+		log.info("DELETE ROWs");
+		IntStream.range(666, 676).forEach(
+				id -> jdbcTemplate.execute("DELETE FROM hr.depts WHERE deptno = " + id)
+		);
 
-			jdbcTemplate.query(
-					"SELECT * FROM hr.depts",
-					(rs, rowNum) -> new Department(rs.getLong("deptno"), rs.getString("department_name"))
-			).forEach(department -> log.info(department.toString()));
-
-
-			log.info("");
-		};
+		jdbcTemplate.query(
+				"SELECT * FROM hr.depts",
+				(rs, rowNum) -> new Department(rs.getLong("deptno"), rs.getString("department_name"))
+		).forEach(department -> log.info(department.toString()));
 	}
 
 	@Bean
-	@ConfigurationProperties("app.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+	@ConfigurationProperties("calcite.datasource")
+	public DataSource dataSource(@Autowired String inlineModelConnection) {
+		log.info("Calcite jdbc url: " + inlineModelConnection);
+
+		return DataSourceBuilder
+				.create()
+				.driverClassName("org.apache.calcite.jdbc.Driver")
+				.url(inlineModelConnection)
+				.build();
 	}
 
+	@Bean
+	public String inlineModelConnection(@Value("${calcite.journalVersionType:TIMESTAMP}") String journalVersionType,
+			@Value("${calcite.journalDefaultKey}") String journalDefaultKey,
+			@Value("${calcite.journalTables}") String journalTables,
+			@Value("${calcite.journalSubsequentVersionField:subsequent_version_number}") String journalSubsequentVersionField,
+			@Value("${calcite.journalVersionField:version_number}") String journalVersionField,
+			@Value("${calcite.journalSuffix:_journal}") String journalSuffix,
+			@Value("${calcite.connectionConfigurationPath}") String connectionConfigurationPath) {
 
-//	@Bean
-//	public DataSource dataSource() {
-//		return DataSourceBuilder
-//				.create()
-//				.username("admin")
-//				.password("admin")
-//				.url("jdbc:calcite:model=sql-rewriter-springboot-example/src/main/resources/myTestModel.json;lex=JAVA")
-//				.driverClassName("org.apache.calcite.jdbc.Driver")
-//				.build();
-//	}
+		String model = "{\n" +
+				"  \"version\": \"1.0\",\n" +
+				"  \"defaultSchema\": \"doesntmatter\",\n" +
+				"  \"schemas\": [\n" +
+				"    {\n" +
+				"      \"name\": \"hr\",\n" +
+				"      \"type\": \"custom\",\n" +
+				"      \"factory\": \"org.apache.calcite.adapter.jdbc.JournalledJdbcSchema$Factory\",\n" +
+				"      \"operand\": {\n" +
+				"        \"connection\": \"" + connectionConfigurationPath + "\",\n" +
+				"        \"jdbcSchema\": \"hr\",\n" +
+				"        \"journalSuffix\": \"" + journalSuffix + "\",\n" +
+				"        \"journalVersionField\": \"" + journalVersionField + "\",\n" +
+				"        \"journalSubsequentVersionField\": \"" + journalSubsequentVersionField + "\",\n" +
+				"        \"journalDefaultKey\": \"" + journalDefaultKey + "\",\n" +
+				"        \"journalVersionType\": \"" + journalVersionType + "\",\n" +
+				"        \"journalTables\": {" + journalTables + "}\n" +
+				"      }\n" +
+				"    }\n" +
+				"  ]\n" +
+				"}";
 
+		return "jdbc:calcite:lex=JAVA;model=inline:" + model;
+	}
 }
