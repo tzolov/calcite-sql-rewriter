fb51c92c07a7dc83d0757f3a04ef2954326a2926
Christian Tzolov <christian.tzolov@gmail.com>
2017-03-18 20:59:56 +0100

    Add test for incorrect version_number type

--- journalled-sql-rewriter/src/test/java/org/apache/calcite/adapter/jdbc/UpdateBigintIntegrationTest.java
+++ journalled-sql-rewriter/src/test/java/org/apache/calcite/adapter/jdbc/UpdateBigintIntegrationTest.java
@@ -21,4 +21,19 @@ public class UpdateBigintIntegrationTest extends IntegrationBase {
 						"FROM \"" + actualSchemaName + "\".\"depts_journal\") AS \"t\"\n" +
 						"WHERE \"version_number\" = \"$f4\" AND \"subsequent_version_number\" IS NULL AND \"deptno\" = 3)", 1);
 	}
+
+	@Test(expected = RuntimeException.class)
+	public void testRewritingWithIncorrectVersionType() {
+		String model = TargetDatabase.makeJournalledModel(versionType);
+		model = model.replace("BIGINT", "TIMESTAMP");
+		CalciteAssert
+				.model(model)
+				.query("UPDATE \"" + virtualSchemaName + "\".\"depts\" SET \"department_name\"='First' WHERE \"deptno\" = 3")
+				.withHook(Hook.PROGRAM, JournalledJdbcRuleManager.program())
+				.planUpdateHasSql("INSERT INTO \"" + actualSchemaName + "\".\"depts_journal\" (\"deptno\", \"department_name\", \"version_number\")\n" +
+						"(SELECT \"deptno\", 'First' AS \"department_name\", \"version_number\" + 1 AS \"version_number\"\n" +
+						"FROM (SELECT \"deptno\", \"version_number\", \"subsequent_version_number\", MAX(\"version_number\") OVER (PARTITION BY \"deptno\" ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS \"$f4\"\n" +
+						"FROM \"" + actualSchemaName + "\".\"depts_journal\") AS \"t\"\n" +
+						"WHERE \"version_number\" = \"$f4\" AND \"subsequent_version_number\" IS NULL AND \"deptno\" = 3)", 1);
+	}
 }
